ff18825a5c346424701ee108a9ac878c741a7672
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-04-28 07:47:21 +0200

    Implement node and tree id parsing.

--- src/main.rs
+++ src/main.rs
@@ -3,67 +3,102 @@ use std::io::{prelude::*, BufReader};
 use regex::Regex;
 
 struct TreeParser {
-    tree_name_regex: Regex,
+    tree_id_regex: Regex,
+    tree_id_finder: Regex,
     node_regex: Regex,
+    node_finder: Regex,
+    node_level_finder: Regex,
     comment_regex: Regex
 }
 
 impl TreeParser {
     fn new() -> TreeParser {
         Self {
-            tree_name_regex: Regex::new(r"^\[[A-Za-z0-9_]+\]$").unwrap(),
+            tree_id_regex: Regex::new(r"^\[[A-Za-z0-9_]+\]$").unwrap(),
+            tree_id_finder: Regex::new(r"[A-Za-z0-9_]+").unwrap(),
             node_regex: Regex::new(r"^(\+ )+[^\+].*$").unwrap(),
+            node_finder: Regex::new(r"(\+ )+").unwrap(),
+            node_level_finder: Regex::new(r"(\+ )").unwrap(),
             comment_regex: Regex::new(r"^#.*+$").unwrap()
         }
     }
 }
 
 enum TreeStatement {
-    TreeName(String),
-    Node(String),
-    Comment(String),
+    TreeID(String),
+    Node(String, u32),
+    Comment,
     Empty,
-    Invalid(String)
+    Invalid
 }
 
 impl TreeStatement {
     fn new(statement: &String, parser: &TreeParser) -> TreeStatement {
         if parser.node_regex.is_match(statement) {
-            TreeStatement::Node(String::from(statement))
+            let n = parser.node_finder.find(statement).unwrap();
+            let node = &statement[n.end()..];
+            let level_iter = parser.node_level_finder.find_iter(statement);
+            let mut level = 0;
+            for _ in level_iter { level += 1 }
+            TreeStatement::Node(String::from(node), level)
         }
-        else if parser.tree_name_regex.is_match(statement) {
-            TreeStatement::TreeName(String::from(statement))
+        else if parser.tree_id_regex.is_match(statement) {
+            let n = parser.tree_id_finder.find(statement).unwrap();
+            let tree_id = &statement[n.start()..n.end()];
+            TreeStatement::TreeID(String::from(tree_id))
         }
         else if parser.comment_regex.is_match(statement) {
-            TreeStatement::Comment(String::from(statement))
+            TreeStatement::Comment
         }
         else {
             if statement.len() == 0 {
                 TreeStatement::Empty
             }
+            else if statement.trim().len() == 0 {
+                TreeStatement::Empty
+            }
             else {
-                TreeStatement::Invalid(String::from(statement))
+                TreeStatement::Invalid
             }
         }
     }
 }
 
+fn parse_tree(reader: BufReader<File>,  parser: &TreeParser) -> Result<String, String> {
+    let mut i = 0;
+    for l in reader.lines() {
+        i += 1;
+        if let Ok(line) = l {
+            match TreeStatement::new(&line, &parser) {
+                TreeStatement::TreeID(s)    => println!("tree_id       {}", s),
+                TreeStatement::Node(s, l)   => println!("node          {} ({})", s, l),
+                TreeStatement::Invalid      => return Result::Err(format!("Invalid statement at line {}", i)),
+                _ => {}
+            }
+        }
+        else {
+           return Result::Err(format!("Could not read line at {}", i));
+        }
+    }
+    //TODO: return the tree structure
+    Result::Ok("".to_string())
+}
+
+/*
+TODO:
+- Genera un dict amb tantes claus com tree names a l'arxiu.
+- Generate simple tree with default object or user defined. La callback de l'usr rep: node str, parent node i depth.
+- Generate reversible tree, nodes have reference of parent.
+- DFS and BFS algorithms.
+*/
+
 fn main() {
     let parser = TreeParser::new();
     if let Ok(file) = File::open("file.tdf") {
-        let reader = BufReader::new(file);
-        for l in reader.lines() {
-            if let Ok(line) = l {
-                match TreeStatement::new(&line, &parser) {
-                    TreeStatement::TreeName(s)  => println!("tree name     {}", s),
-                    TreeStatement::Node(s)      => println!("node          {}", s),
-                    TreeStatement::Comment(s)   => println!("comment       {}", s),
-                    TreeStatement::Empty        => println!("empty"),
-                    TreeStatement::Invalid(s)   => println!("invalid       {}", s),
-                }
-            }
-        }
+        let r = parse_tree(BufReader::new(file), &parser);
+        println!("{:?}", r);
     }
-
-    println!("EOF.");
+    else {
+        println!("Could not read file");
+    }
 }
