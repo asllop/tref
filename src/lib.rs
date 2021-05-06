5f28d7b10852ec7aab00ee1945a8fe10d2c57c16
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-05-06 17:41:58 +0200

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -270,6 +270,7 @@ pub struct Forest {
 }
 
 impl Forest {
+    //TODO: return an error type implementing the std::error::Error trait.
     pub fn new(reader: BufReader<impl Read>) -> Result<Self, String> {
         let parser = parser::TreeParser::new();
         let mut stack = stack::NodeStack::new();
@@ -413,3 +414,48 @@ TODO:
 - Allow using the BufReader directly to read data from the tree, instead of parsing and generating a model in mem. For very big trees.
 - Generate a tree/forest programatically and serialize into a TREF file.
 */
+
+#[cfg(test)]
+mod tests {
+    use std::io::BufReader;
+
+    #[test]
+    fn check_forest_generation() {
+        let tref_sample =
+        "[test_tree]\n\
+        + root_node\n\
+        + + child_1\n\
+        + + child_2\n\
+        + + + child_2_1\n\
+        + + + + child_2_1_1\n";
+    
+        match crate::Forest::new(BufReader::new(tref_sample.as_bytes())) {
+            Ok(mut forest) => {
+                if let Some(tree_model) = forest.tree(&String::from("test_tree")) {
+                    for (i,n) in tree_model.bfs_iter().enumerate() {
+                        match i {
+                            0 => {
+                                if !n.content.eq("root_node") { panic!("Wrong root node content!"); }
+                                if let Some(_) = n.parent_position { panic!("Root node has a parent!"); }
+                                if n.children.len() != 2 { panic!("Root node hasn't 3 children!"); }
+                                if n.children[0] != 1 || n.children[1] != 2 { panic!("Root node children are incorrect!"); }
+                            },
+                            1 => {
+                                if !n.content.eq("child_1") { panic!("Wrong node 1 content!"); }
+                                if let None = n.parent_position { panic!("Node 1 has a no parent!"); }
+                                if let Some(parent_n) = n.parent_position {
+                                    if parent_n != 0 {
+                                        panic!("Node 1 has wrong parent!");
+                                    }
+                                }
+                                if n.children.len() != 0 { panic!("Node 1 hasn't 0 children!"); }
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            },
+            Err(msg) => panic!("ERROR = {}", msg)
+        }
+    }
+}
