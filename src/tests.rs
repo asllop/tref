0f32e42e6ea449b7461280c32f863330ef19b8fb
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-05-07 12:30:41 +0200

    Improve tests.

--- src/tests.rs
+++ src/tests.rs
@@ -1,14 +1,16 @@
 use std::io::BufReader;
 
 #[test]
-fn check_forest_generation() {
+fn check_forest_integrity() {
     let tref_sample =
     "[test_tree]\n\
     + root_node\n\
     + + child_1\n\
     + + child_2\n\
     + + + child_2_1\n\
-    + + + + child_2_1_1\n";
+    + + + + child_2_1_1\n\
+    + + + child_2_2\n\
+    + + child_3\n";
 
     match crate::Forest::new(BufReader::new(tref_sample.as_bytes())) {
         Ok(forest) => {
@@ -16,53 +18,73 @@ fn check_forest_generation() {
                 for (i,n) in tree_model.bfs_iter().enumerate() {
                     match i {
                         0 => {
-                            if !n.content.eq("root_node") { panic!("Wrong root node content!"); }
-                            if let Some(_) = n.parent_position { panic!("Root node has a parent!"); }
-                            if n.children.len() != 2 { panic!("Root node hasn't 3 children!"); }
-                            if n.children[0] != 1 || n.children[1] != 2 { panic!("Root node children are incorrect!"); }
+                            if !n.content.eq("root_node") { panic!("Wrong root_node content!"); }
+                            if let Some(_) = n.parent_position { panic!("root_node has a parent!"); }
+                            if n.children.len() != 3 { panic!("root_node hasn't 3 children!"); }
+                            if n.children[0] != 1 || n.children[1] != 2 || n.children[2] != 6 { panic!("root_node children are incorrect!"); }
                         },
                         1 => {
-                            if !n.content.eq("child_1") { panic!("Wrong node 1 content!"); }
-                            if let None = n.parent_position { panic!("Node 1 has a no parent!"); }
+                            if !n.content.eq("child_1") { panic!("Wrong child_1 content!"); }
+                            if let None = n.parent_position { panic!("child_1 has a no parent!"); }
                             if let Some(parent_n) = n.parent_position {
                                 if parent_n != 0 {
-                                    panic!("Node 1 has wrong parent!");
+                                    panic!("child_1 has wrong parent!");
                                 }
                             }
-                            if n.children.len() != 0 { panic!("Node 1 hasn't 0 children!"); }
+                            if n.children.len() != 0 { panic!("child_1 hasn't 0 children!"); }
                         },
                         2 => {
-                            if !n.content.eq("child_2") { panic!("Wrong node 2 content!"); }
-                            if let None = n.parent_position { panic!("Node 2 has a no parent!"); }
+                            if !n.content.eq("child_2") { panic!("Wrong child_2 content!"); }
+                            if let None = n.parent_position { panic!("child_2 has a no parent!"); }
                             if let Some(parent_n) = n.parent_position {
                                 if parent_n != 0 {
-                                    panic!("Node 2 has wrong parent!");
+                                    panic!("child_2 has wrong parent!");
                                 }
                             }
-                            if n.children.len() != 1 { panic!("Node 2 hasn't 1 child!"); }
-                            if n.children[0] != 3 { panic!("Node 2 children are incorrect!"); }
+                            if n.children.len() != 2 { panic!("child_2 hasn't 2 children!"); }
+                            if n.children[0] != 3 || n.children[1] != 5 { panic!("child_2 children are incorrect!"); }
                         },
                         3 => {
-                            if !n.content.eq("child_2_1") { panic!("Wrong node 3 content!"); }
-                            if let None = n.parent_position { panic!("Node 3 has a no parent!"); }
+                            if !n.content.eq("child_3") { panic!("Wrong child_3 content!"); }
+                            if let None = n.parent_position { panic!("child_3 has a no parent!"); }
                             if let Some(parent_n) = n.parent_position {
-                                if parent_n != 2 {
-                                    panic!("Node 3 has wrong parent!");
+                                if parent_n != 0 {
+                                    panic!("child_3 has wrong parent!");
                                 }
                             }
-                            if n.children.len() != 1 { panic!("Node 3 hasn't 1 child!"); }
-                            if n.children[0] != 4 { panic!("Node 3 children are incorrect!"); }
+                            if n.children.len() != 0 { panic!("child_3 hasn't 0 children!"); }
                         },
                         4 => {
-                            if !n.content.eq("child_2_1_1") { panic!("Wrong node 4 content!"); }
-                            if let None = n.parent_position { panic!("Node 4 has a no parent!"); }
+                            if !n.content.eq("child_2_1") { panic!("Wrong child_2_1 content!"); }
+                            if let None = n.parent_position { panic!("child_2_1 has a no parent!"); }
                             if let Some(parent_n) = n.parent_position {
-                                if parent_n != 3 {
-                                    panic!("Node 4 has wrong parent!");
+                                if parent_n != 2 {
+                                    panic!("child_2_1 has wrong parent!");
                                 }
                             }
-                            if n.children.len() != 0 { panic!("Node 4 hasn't 0 children!"); }
+                            if n.children.len() != 1 { panic!("child_2_1 hasn't 1 child!"); }
+                            if n.children[0] != 4 { panic!("child_2_1 children are incorrect!"); }
                         },
+                        5 => {
+                            if !n.content.eq("child_2_2") { panic!("Wrong child_2_2 content!"); }
+                            if let None = n.parent_position { panic!("child_2_2 has a no parent!"); }
+                            if let Some(parent_n) = n.parent_position {
+                                if parent_n != 2 {
+                                    panic!("child_2_2 has wrong parent!");
+                                }
+                            }
+                            if n.children.len() != 0 { panic!("child_2_2 hasn't 0 children!"); }
+                        },
+                        6 => {
+                            if !n.content.eq("child_2_1_1") { panic!("Wrong child_2_1_1 content!"); }
+                            if let None = n.parent_position { panic!("child_2_1_1 has a no parent!"); }
+                            if let Some(parent_n) = n.parent_position {
+                                if parent_n != 3 {
+                                    panic!("child_2_1_1 has wrong parent!");
+                                }
+                            }
+                            if n.children.len() != 0 { panic!("child_2_1_1 hasn't 0 children!"); }
+                        }
                         _ => {}
                     }
                 }
