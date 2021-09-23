c25d915848b146aa422105f179d5d51c73888a6c
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-09-23 10:58:42 +0200

    Improve main.

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,25 @@ use tref;
 fn main() {
     if let Ok(file) = File::open("file.tref") {
         let model = <tref::Model>::new();
-        if let Ok(forest) = model.parse(BufReader::new(file)) {
-            if let Some(tree) = forest.get_tree("my_tree") {
-                println!("my_tree = {:#?}", tree);
-            }
-
-            let f = File::create("serialized.tref").expect("Unable to create file");
-            let mut buf_writer = BufWriter::new(f);
-            match model.serialize(&forest, &mut buf_writer) {
-                Ok(num_lines) => {
-                    println!("Tree serialized correctly, num lines = {}", num_lines);
-                },
-                Err(e) => {
-                    println!("Failed serializing tree: {}", e);
+        match model.parse(BufReader::new(file)) {
+            Ok(forest) => {
+                if let Some(tree) = forest.get_tree("my_tree") {
+                    println!("my_tree = {:#?}", tree);
                 }
+
+                let f = File::create("serialized.tref").expect("Unable to create file");
+                let mut buf_writer = BufWriter::new(f);
+                match model.serialize(&forest, &mut buf_writer) {
+                    Ok(num_lines) => {
+                        println!("Tree serialized correctly, num lines = {}", num_lines);
+                    },
+                    Err(e) => {
+                        println!("Failed serializing tree: {}", e);
+                    }
+                }
+            },
+            Err(e) => {
+                println!("Failed parsing document: {}", e);
             }
         }
     }
