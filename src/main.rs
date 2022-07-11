54048b64201fd272902c0d63c8ce90864af9a077
Andreu <andreu.santaren@gmail.com>
2022-07-11 14:18:27 +0200

    Improve usage example.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use std::{fs::File, io::{BufReader, BufWriter}};
-use tref::Model as TrefModel;
+use tref;
 
 fn main() {
     if let Ok(file) = File::open("file.tref") {
-        match <TrefModel>::parse(BufReader::new(file)) {
+        match <tref::Model>::parse(BufReader::new(file)) {
             Ok(forest) => {
-                if let Some(tree) = forest.get_tree("My Original Tree!") {
-                    println!("my_tree = {:#?}", tree);
-                }
-
-                let f = File::create("serialized.tref").expect("Unable to create file");
-                let mut buf_writer = BufWriter::new(f);
-                match <TrefModel>::serialize(&forest, &mut buf_writer) {
-                    Ok(num_lines) => {
-                        println!("Tree serialized correctly, num lines = {}", num_lines);
-                    },
-                    Err(e) => {
-                        println!("Failed serializing tree: {}", e);
+                for (tree_id, tree) in forest.iter() {
+                    println!("{} = {:#?}", tree_id, tree);
+    
+                    let f = File::create("serialized.tref").expect("Unable to create file");
+                    let mut buf_writer = BufWriter::new(f);
+                    match <tref::Model>::serialize(&forest, &mut buf_writer) {
+                        Ok(num_lines) => {
+                            println!("Tree serialized correctly, num lines = {}", num_lines);
+                        },
+                        Err(e) => {
+                            println!("Failed serializing tree: {}", e);
+                        }
                     }
+
+                    println!("--------------------------");
                 }
             },
             Err(e) => {
